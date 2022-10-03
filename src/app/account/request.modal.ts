edcfe290887c2843ca7536be6416f861d62f747e
Clarence <ecneralcnihc@gmail.com>
2022-10-03 22:55:20 +0800

    updated

--- src/app/account/request.modal.ts
+++ src/app/account/request.modal.ts
@@ -1,6 +1,10 @@
 export interface Request{
+  
   description: string;
   datetime: Date;
   studentlevel: string;
   numofexpectedstudents: string;
+  status: string;
+  school_name: string;
+  city: string;
 }
--- src/app/admin/submit-request/submit-request.component.ts
+++ src/app/admin/submit-request/submit-request.component.ts
@@ -2,6 +2,7 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
 import {Component} from '@angular/core';
 import { AccService } from 'src/app/account/account.service';
 import { NgForm } from '@angular/forms';
+import { from } from 'rxjs';
 
 @Component({
   selector:'app-admin-submit-request',
@@ -26,8 +27,8 @@ export class SubmitRequestComponent{
       if(form.invalid){
         return;
       }
-
-      this.accService.AddRequest(form.value.description, form.value.datetime, form.value.studentlevel, form.value.numofexpectedstudents);
+   
+      this.accService.AddRequest(form.value.description, form.value.datetime, form.value.studentlevel, form.value.numofexpectedstudents, form.value.status='NEW', form.value.school_name='', form.value.city='');
       form.resetForm();
   
 
